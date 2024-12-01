542be21cd1b558b2b1d01d423b2fe294767514de
Hemdan47 <mohammedahmed98731@gmail.com>
2024-12-01 23:13:27 +0200

    adding initial implementation for the fcai scheduler algorithm

--- src/main/java/com/fcai/Main/Main.java
+++ src/main/java/com/fcai/Main/Main.java
@@ -1,10 +1,13 @@
 package com.fcai.Main;
+import com.fcai.FCAI.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
     @Override
@@ -19,6 +22,17 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 //        launch();
-        System.out.println("Hello World");
+
+        Process p1 = new Process("p1" , "first" , 17 , 0 , 4 , 4);
+        Process p2 = new Process("p2" , "second" , 6 , 3 , 9 , 3);
+        Process p3 = new Process("p3" , "third" , 10 , 4 , 3 , 5);
+        Process p4 = new Process("p4" , "fourth" , 4 , 29 , 2 , 2);
+        List<Process> list = List.of(p1,p2,p3,p4);
+        Scheduler s = new FCAI(list);
+        s.addProcess(p1);
+        s.addProcess(p2);
+        s.addProcess(p3);
+        s.addProcess(p4);
+        s.execute();
     }
 }
--- src/main/java/com/fcai/Main/Scheduler.java
+++ src/main/java/com/fcai/Main/Scheduler.java
@@ -3,13 +3,11 @@ import java.util.Collection;
 import java.util.List;
 
 public abstract class Scheduler {
-    private Collection<Process> processList;
-    private int contextSwitchingTime;
-
+    protected List<Process> processList;
+    protected int contextSwitchingTime;
     public boolean addProcess(Process process) {
         return processList.add(process);
     }
-
     public int calculateAverageWaitingTime() {
         int totalWaitingTime = 0;
         for (Process process : processList) {
@@ -24,11 +22,9 @@ public abstract class Scheduler {
         }
         return (int)Math.ceil(totalTurnAroundTime/processList.size());
     }
-
     public int getContextSwitchingTime() {
         return contextSwitchingTime;
     }
-
     public abstract void execute();
 
 
