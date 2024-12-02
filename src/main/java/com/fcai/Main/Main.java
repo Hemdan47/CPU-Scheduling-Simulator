e06d544616d9368529acf27f3f864df98d0f5c97
Salmamohamedd <125445879+Salmamohamedd@users.noreply.github.com>
2024-12-03 01:10:03 +0200

    Update Main.java
    Main method for running the SJF class

--- src/main/java/com/fcai/Main/Main.java
+++ src/main/java/com/fcai/Main/Main.java
@@ -1,5 +1,6 @@
 package com.fcai.Main;
 import com.fcai.FCAI.*;
+import com.fcai.SJF.SJF;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
@@ -8,8 +9,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-public class Main extends Application {
+public class Main extends Application{
     @Override
     public void start(Stage stage) throws IOException {
         Text t = new Text("Hello World");
@@ -24,15 +24,21 @@ public class Main extends Application {
 //        launch();
 
         Process p1 = new Process("p1" , "first" , 17 , 0 , 4 , 4);
-        Process p2 = new Process("p2" , "second" , 6 , 3 , 9 , 3);
+        Process p2 = new Process("p2" , "second" , 6 , 0 , 9 , 3);
         Process p3 = new Process("p3" , "third" , 10 , 4 , 3 , 5);
         Process p4 = new Process("p4" , "fourth" , 4 , 29 , 2 , 2);
+
+//        Process p1 = new Process("p1" , "first" , 7 , 0 , 4 , 4);
+//        Process p2 = new Process("p2" , "second" , 4 , 2 , 9 , 3);
+//        Process p3 = new Process("p3" , "third" , 1 , 4 , 3 , 5);
+//        Process p4 = new Process("p4" , "fourth" , 4 , 5 , 2 , 2);
+
         List<Process> list = List.of(p1,p2,p3,p4);
-        Scheduler s = new FCAI(list);
-        s.addProcess(p1);
-        s.addProcess(p2);
-        s.addProcess(p3);
-        s.addProcess(p4);
+        Scheduler s = new SJF(list);
+//        s.addProcess(p1);
+//        s.addProcess(p2);
+//        s.addProcess(p3);
+//        s.addProcess(p4);
         s.execute();
     }
 }
